de5c1dbde7832da31aa6c45e390aaeb623808810
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-09-12 22:16:30 +0530

    feat: divide solution

---
+++ Exercises/InsertionSort/divide.java
@@ -0,0 +1,11 @@
+public class divide {
+    int nums = 10;
+
+    static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divide(10, 2));
+    }
+}
