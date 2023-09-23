d9941d94959df78cf33d33b62fbb3196584f3ffd
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-09-23 23:22:15 +0530

    feat: QuickSort implementaiton completed

--- Exercises/sorting/QuickSort.java
+++ Exercises/sorting/QuickSort.java
@@ -1,17 +1,46 @@
 public class QuickSort {
-    public int[] quickSort(int[] arr , int low, int high) {
-        return arr;
+    public void quickSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int pivotIdx = partition(arr, low, high);
+        quickSort(arr, low, pivotIdx - 1);
+        quickSort(arr, pivotIdx + 1, high);
     }
+
     private int partition(int[] arr, int low, int high) {
-        return 0;
+        int pivot = arr[high];
+        int idx = low - 1;
+        for (int i = low; i < high; i++) {
+            if (arr[i] < pivot) {
+                idx++;
+                swap(arr, idx, i);
+            }
+        }
+
+        idx++;
+        arr[high] = arr[idx];
+        arr[idx] = pivot;
+        return idx;
     }
+
+    private void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void main(String[] args) {
 
-        qs = new QuickSort();
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-        int[] sortedArr = qs.quickSort(arr, 0, arr.length - 1);
-        for (int i = 0; i < sortedArr.length; i++) {
-            System.out.println(sortedArr[i]);
+        System.out.println("Hello World!");
+        QuickSort qs = new QuickSort();
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 0 };
+        qs.quickSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
 
     }
