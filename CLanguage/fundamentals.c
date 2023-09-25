fdf9832ce31caa86f0dd0641fff74970025b6bf4
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-09-25 23:35:47 +0530

    feat: c fundamentals

---
+++ CLanguage/fundamentals.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <stdio.h>
+int lower(int c) {
+  if (c >= 'A' && c <= 'Z')
+    return c + 'a' - 'A';
+  else
+    return c;
+}
+int main() {
+  int e = lower('A');
+
+  char esc = e;
+  char res = tolower(res);
+  printf("%c\n", esc);
+}
