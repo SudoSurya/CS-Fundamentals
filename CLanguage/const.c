10d41d14c7aea6cab614748fc98b76b5fd44f595
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-09-28 18:06:31 +0530

    git ignore modified

--- CLanguage/const.c
+++ CLanguage/const.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
+int len(const int a) { return a; }
 
-int len(const int a){
-    return a;
-}
-
-int main(){
-    int a = 10;
-    int res = len(a);
-    printf("%d\n", res);
+int main() {
+  int a = 10;
+  int res = len(a);
+  printf("%d\n", res);
+  for (int i = 0; i < 6; i++) {
+    printf("%d\n", i);
+  }
 }
 
 // Path: const.c
-
-
