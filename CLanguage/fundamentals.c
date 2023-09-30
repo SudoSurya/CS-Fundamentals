241871761b72af9d3c88ec62485ad47828640460
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-09-30 12:08:13 +0530

    feat: loops

--- CLanguage/fundamentals.c
+++ CLanguage/fundamentals.c
@@ -6,16 +6,32 @@ int lower(int c) {
   else
     return c;
 }
+int power(int base, int n) {
+  int p;
+  for (p = 1; n > 0; --n)
+    p = p * base;
+  return p;
+}
 int main() {
   int e = lower('A');
 
   char esc = e;
   char res = tolower(res);
-  printf("%c\n", esc);
 
   int n = n & 0177;
-  printf("%d\n", n);
-}
+  int a = 10;
+  // printf("%d+%d", a, power(2, 3));
 
+  int x = 10;
+  int y = 20;
+
+  int z = (x > y) ? x : y;
+  // printf("biggest val is %d\n", z);
+  // printf("biggest val is %d\n", z);
+  // printf("biggest val is %d\n", z);
+  for (int i = 1; i <= 100; i++) {
+    printf("%5d%c", i, (i % 10 == 0 || i == n - 1) ? '\n' : ' ');
+  }
+}
 
 // Path: fundamentals.c
