4bd1ded3e480eef6f03e3d7770ccdfaae41ef3b9
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-10-19 22:04:41 +0530

    feat: gcd of a number

--- GoLang/variables/main.go
+++ GoLang/variables/main.go
@@ -13,11 +13,20 @@ func main() {
 	b := 1
 
 	fmt.Println(p == foo)
-
+    g()
 	fmt.Println(&a == &b)
+	result := Gcd(10, 20)
+	fmt.Println(result)
 }
 func g() {
 	y := new(int)
 	*y = 1
-    // y doesnt escape from g so it is allocated on the stack
+	// y doesnt escape from g so it is allocated on the stack
+}
+
+func Gcd(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
 }
