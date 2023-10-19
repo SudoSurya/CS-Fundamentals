5902e1566cc521ff013a13278765c2f3ebe40582
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-10-19 21:40:16 +0530

    feat: flags package

--- GoLang/hello.go
+++ GoLang/hello.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 )
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+
+	z := new(int)
+    fmt.Println("ss",&z)
+	pop := new(int)
+    fmt.Println("ss",&pop)
+	foo := new(int)
+    fmt.Println("ss",&foo)
+	bar := new(int)
+    fmt.Println("ss",&bar)
 
 	// s, sep := "", ""
 	// for& _, arg := range os.Args[1:] {
