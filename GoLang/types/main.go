0a21b793082849ad18ba6ae1987f88194375f0e4
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-10-28 20:17:32 +0530

    feat: go

--- GoLang/types/main.go
+++ GoLang/types/main.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func a() {}
+
 type Celsius float64
 type Fahrenheit float64
 
+var cwd string
+
+func init() {
+    var err error
+	cwd, err = os.Getwd() // NOTE: wrong!
+	if err != nil {
+		log.Fatalf("os.Getwd failed: %v", err)
+	}
+	log.Printf("Working directory = %s", cwd)
+}
 func main() {
+	var a Celsius = 1.0
+	var b Fahrenheit = 1.0
+	println(a == Celsius(b))
+	x := "hello"
+	for _, x := range x {
+		x := x + 'a' - 'A'
+		fmt.Printf("%c", x)
+	}
 }
