66a3f446dd338cad8aedf08c3b3f325b518902e6
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-11-02 16:43:29 +0530

    feat: Slices and Maps Basics

--- GoLang/arrays/main.go
+++ GoLang/arrays/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Currency int
 
@@ -46,4 +48,48 @@ func main() {
 		fmt.Printf("%d %d\n", i, v)
 	}
 	fmt.Printf("%x", newArr)
+	fmt.Println("------------------------------")
+	exSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	exSlice1 := []int{1, 2, 3, 5, 5, 6, 7, 8, 9}
+	fmt.Println(equal(exSlice, exSlice1))
+	nilSlice := []int{}
+	if nilSlice == nil {
+		fmt.Println("nil")
+	} else {
+		fmt.Println("not nil")
+	}
+    // NOTE: slice is a reference type
+	ages := make(map[string]int)
+	ages["alice"] = 31
+	ages["charlie"]++
+	ages["charlie"]++
+	ages["charlie"]++
+	ages["charlie"]++
+	ages["charlie"]++
+	ages["charlie"]++
+	ages["charlie"]++
+    fmt.Println(ages["charlie"])
+	fmt.Println("------------------------------")
+	newAges := map[string]int{
+		"alice":   31,
+		"charlie": 34,
+		"bob":     20,
+	}
+    for name, age := range newAges {
+        fmt.Printf("%s\t%d\n", name, age)
+    }
+	fmt.Println("------------------------------")
+    delete(newAges, "bob")
+}
+
+func equal(x, y []int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
 }
